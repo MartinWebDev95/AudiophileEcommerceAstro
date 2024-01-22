a66d7118387a84e16f63e13ec031f8e9112ef7c3
Juan Antonio <jamll.muro@gmail.com>
2024-01-22 17:38:23 +0100

    Add a file that return all nav links

---
+++ src/utils/navItems.js
@@ -0,0 +1,28 @@
+const navItems = [
+  {
+    navTo: '/',
+    image: null,
+    navName: 'Home',
+    shop: false,
+  },
+  {
+    navTo: '/headphones',
+    image: '/assets/shared/desktop/image-headphones.png',
+    navName: 'Headphones',
+    shop: true,
+  },
+  {
+    navTo: '/speakers',
+    image: '/assets/shared/desktop/image-speakers.png',
+    navName: 'Speakers',
+    shop: true,
+  },
+  {
+    navTo: '/earphones',
+    image: '/assets/shared/desktop/image-earphones.png',
+    navName: 'Earphones',
+    shop: true,
+  },
+];
+
+export default navItems;
