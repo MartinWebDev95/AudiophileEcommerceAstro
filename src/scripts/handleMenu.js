e84b7b9c6cc398bf4e6edac647120e8b3b17d9c0
Juan Antonio Martín <jamll.muro@gmail.com>
2025-07-14 13:58:18 +0200

    Change the handleMenu script

--- src/scripts/handleMenu.js
+++ src/scripts/handleMenu.js
@@ -1,15 +1,4 @@
-/* Check if the code is running on a mobile or a PC, because mobile browsers don't support view transitions */
-if (navigator.userAgent.match(/Android/i) 
-|| navigator.userAgent.match(/webOS/i) 
-|| navigator.userAgent.match(/iPhone/i) 
-|| navigator.userAgent.match(/iPad/i) 
-|| navigator.userAgent.match(/iPod/i) 
-|| navigator.userAgent.match(/BlackBerry/i) 
-|| navigator.userAgent.match(/Windows Phone/i)) {
-  menu();
-} else {
-  document.addEventListener('astro:page-load', menu);
-}
+document.addEventListener('astro:page-load', menu);
 
 function menu(){
   const btnMobile = document.querySelector('.menuMobile');
